24d34f380ce26ccf50853bd1dee80e25257c0534
lawjwpark <81797681+lawjwpark@users.noreply.github.com>
2023-10-10 11:56:58 +0000

    Solve with dynamic programming

--- Gold 5/#12865 : 평범한 배낭.py
+++ Gold 5/#12865 : 평범한 배낭.py
@@ -1,12 +1,12 @@
 def knapsack(n, k, weights, values):
-    dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
     
-    for i in range(n + 1):
-        for j in range(k + 1):
-            if i == 0 or j == 0:
-                dp[i][j] = 0
-            elif weights[i - 1] <= j:
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + values[i - 1])
+    for i in range(1, n + 1):
+        for w in range(1, k + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][w] = dp[i - 1][w]
                 
     return dp[n][k]
 
