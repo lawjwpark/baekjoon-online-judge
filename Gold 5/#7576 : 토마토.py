9709db64a5b9867a9f62ffc2e93d3d6e8d4ec40b
lawjwpark <81797681+lawjwpark@users.noreply.github.com>
2023-10-10 11:12:47 +0000

    Solve with BFS

--- Gold 5/#7576 : 토마토.py
+++ Gold 5/#7576 : 토마토.py
@@ -1,30 +1,32 @@
 from collections import deque
 
+def bfs(box, ripe):
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    q = deque(ripe)
+    days = -1
+    
+    while q:
+        days += 1
+        for _ in range(len(q)):
+            x, y = q.popleft()
+            
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if 0 <= nx < N and 0 <= ny < M and box[nx][ny] == 0:
+                    box[nx][ny] = 1
+                    q.append((nx, ny))
+
+    for i in range(N):
+        for j in range(M):
+            if box[i][j] == 0:
+                return -1
+
+    return days
+
 M, N = map(int, input().split())
 box = [list(map(int, input().split())) for _ in range(N)]
 ripe = [(i, j) for i in range(N) for j in range(M) if box[i][j] == 1]
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-q = deque(ripe)
-days = -1
-
-while q:
-    days += 1
-    for _ in range(len(q)):
-        x, y = q.popleft()
-    
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and box[nx][ny] == 0:
-                box[nx][ny] = 1
-                q.append((nx, ny))
-
-for i in range(N):
-    for j in range(M):
-        if box[i][j] == 0:
-            print(-1)
-            exit()
-
-print(days)
+print(bfs(box, ripe))
