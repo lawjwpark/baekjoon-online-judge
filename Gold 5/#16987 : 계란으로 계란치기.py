ac0342ab77386379dd409d9239174e02ec9524d5
lawjwpark <81797681+lawjwpark@users.noreply.github.com>
2023-10-12 09:51:18 +0000

    Solve with recursion? back tracking?

--- Gold 5/#16987 : 계란으로 계란치기.py
+++ Gold 5/#16987 : 계란으로 계란치기.py
@@ -1,51 +1,31 @@
-def solution(N, S, W):
+def solution(N, eggs):
     def f(eggs, idx):
-        eggs_copy = eggs.copy()
-        eggs_next_list = []
-        
-        for i, egg in enumerate(eggs):
-            if i != idx and egg[2]:
-                eggs_copy[idx][0] -= eggs_copy[i][1]
-                eggs_copy[i][0] -= eggs_copy[idx][1]
-                
-                if eggs_copy[idx][0] <= 0:
-                    eggs_copy[idx][2] = 0
-                if eggs_copy[i][0] <= 0:
-                    eggs_copy[i][2] = 0
-                    
-                for j in range(idx + 1, N):
-                    if eggs_copy[j][2]:
-                        eggs_next_list.append(f(eggs_copy, j).copy())
-                        break
-                    if j == N - 1:
-                        eggs_next_list.append(eggs_copy)
+        if idx == N:
+            result_list.append(sum(1 for e in eggs if e[0] <= 0))
+            return
+        if eggs[idx][0] <= 0:
+            f(eggs, idx + 1)
+            return
             
-        if not eggs_next_list:
-            return eggs_copy
+        for i in range(N):
+            if i != idx and eggs[i][0] > 0:
+                eggs_copy = eggs.copy()
+                eggs_copy[idx] = (eggs_copy[idx][0] - eggs_copy[i][1], eggs_copy[idx][1])
+                eggs_copy[i] = (eggs_copy[i][0] - eggs_copy[idx][1], eggs_copy[i][1])
 
-        print(f"eggs {eggs}\nidx {idx}\neggs_next_list {eggs_next_list}")
+                f(eggs_copy, idx + 1)
+            elif i == N - 1:
+                result_list.append(sum(1 for e in eggs if e[0] <= 0))
         
-        max_cnt, max_idx = -1, -1
-        for k, eggs_next in enumerate(eggs_next_list):
-            cnt = sum([1 - e[2] for e in eggs_next])
-            if cnt > max_cnt:
-                max_cnt = cnt
-                max_idx = k
-
-        return eggs_next_list[max_idx]
+    result_list = []
+    f(eggs, 0)
     
-    eggs = [[S[i], W[i], 1] for i in range(N)]
-    eggs_max = f(eggs, 0)
-    print(eggs_max)
-    
-    return sum([1 - egg[2] for egg in eggs_max])
-    
+    return max(result_list)
 
 N = int(input())
-S, W = [], []
+eggs = []
 for _ in range(N):
-    s, w = map(int, input().split())
-    S.append(s)
-    W.append(w)
+    S, W = map(int, input().split())
+    eggs.append((S, W))
 
-print(solution(N, S, W))
+print(solution(N, eggs))
