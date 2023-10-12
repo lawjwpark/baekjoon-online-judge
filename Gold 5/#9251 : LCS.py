e1795cf01763c4059ac6b1162b48afa876d20012
lawjwpark <81797681+lawjwpark@users.noreply.github.com>
2023-10-12 15:26:44 +0000

    Solve with dynamic programming

--- Gold 5/#9251 : LCS.py
+++ Gold 5/#9251 : LCS.py
@@ -5,7 +5,6 @@ def solution(s1, s2):
         for j in range(1, len(s2) + 1):
             if s1[i - 1] == s2[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
-                
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
     
